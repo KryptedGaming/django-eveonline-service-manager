89f34ff6304a868a99ec6ca90bcfbfc33392959c
porowns <porowns@gmail.com>
2020-03-23 11:05:11 -0400

    Update boolean admin fields

--- django_eveonline_service_manager/admin.py
+++ django_eveonline_service_manager/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import EvePayment, EveInvoice, EveServiceTemplate, EveService
+from django.utils import timezone
 
 admin.site.register(EvePayment)
-admin.site.register(EveInvoice)
 admin.site.register(EveServiceTemplate)
-admin.site.register(EveService)
+
+
+@admin.register(EveInvoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'paid')
+
+@admin.register(EveService)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'expires', 'paid')
+
+    def paid(self, obj):
+        expires_in = obj.expires - timezone.now()
+        days = expires_in.days 
+        if days < 5:
+            return False 
+        return True  
+        
+    paid.boolean = True
